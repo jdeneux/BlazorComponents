448f49c7c1cc11f94aac5bf83c46ccfb8e877f37
Jujux <jdeneux@gmail.com>
2019-03-31 13:52:04 +0400

    first commit

---
+++ src/BlazorComponents/Extensions/ElementRefExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorComponents.Extensions
+{
+    public static class ElementRefExtensions
+    {
+        public static Task Focus(this ElementRef elementRef, IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeAsync<object>(
+                "Extensions.focusElement", elementRef);
+        }
+    }
+}
